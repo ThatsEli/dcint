a7957b40c154cd2b1c4de9033eec7d69e559bff8
ThatsEli <public@thatseliyt.de>
2018-02-21 19:58:31 +0100

    working chain link

--- test/test.js
+++ test/test.js
@@ -6,29 +6,26 @@ var node2 = dcint.createNode();
 
 node1.setupEncryptionKey("joO4chN1tmsH8cRF0HeprOd2kwf7GDli");
 
-// node2.setupEncryptionKey("joO4chN1tmsH8cRF0HeprOd2kwf7GDli");
+node2.setupEncryptionKey("joO4chN1tmsH8cRF0HeprOd2kwf7GDli");
 
 node1.initNode(4000);
 
 node2.initNode(5000);
 
 
-node1.attachToNodes(['localhost:5000'], function(data) {
+node1.attachToNodes(['localhost:5000'], function(channel, data) {
+    // console.log(channel + "|" + data);
     console.log(data);
 });
 
-node2.attachToNodes(['localhost:4000'], function(data) {
+node2.attachToNodes(['localhost:4000'], function(channel, data) {
+    // console.log(channel + "|" + data);
     console.log(data);
 });
 
 setTimeout(function() {
-    console.log("Node1: " + node1.getConnectedNodes());
-    console.log("Node2: " + node2.getConnectedNodes());
-}, 500);
-
-setTimeout(function() {
-    node1.emitData({test1:true});
-    node2.emitData({test2:true});
+    node1.emitData('test', {test1:true});
+    node2.emitData('test', {test2:true});
 }, 2000);
 
 
